f0c3a6126a117e5698a2aec7e4b3a6b3c4f7e850
Louis <thatmexikan@live.com>
2014-02-27 12:10:04 -0700

    C# Project Added
    - C# Project added.
    - Interface can now call ruby script

--- ssh_pipe.rb
+++ ssh_pipe.rb
@@ -9,16 +9,18 @@ require 'net/ssh'
 @commands = nil
 
 # read commands from file
-file = File.open("Scripts\\show_all_rules.sh", "r")
+file = File.open("C:\\Users\\Standard Guy\\Desktop\\Scripts\\show_all_rules.sh", "r")
 @commands = file.read
 file.close
 
 begin
 	ssh = Net::SSH.start(@hostname, @username, :password => @password)
 	commandOutput = ssh.exec!(@commands)
-	puts commandOutput
+	puts commandOutput 
 	ssh.close
 rescue
 	puts "Unable to connect to #{@hostname}"
 end
+
+sleep(10)
   
